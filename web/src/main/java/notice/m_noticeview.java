9c30b2cf0c2621f95107a181b43606b1d2fed477
koona-0 <nayoung9909@naver.com>
2025-03-10 17:30:10 +0900

    게시판 삭제 기능 추

--- web/src/main/java/notice/m_noticeview.java
+++ web/src/main/java/notice/m_noticeview.java
@@ -3,6 +3,7 @@ package notice;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import shop.m_dbinfo;
 
@@ -16,6 +17,8 @@ public class m_noticeview {
 	String sql = "";	//Query문법
 	m_dbinfo db = new m_dbinfo();	//Database 정보 
 	
+	ArrayList<String> db_data = null;	//한개의 데이터 그룹만 저장시킴 
+	
 	public void viewcount(int nidx) {
 		
 		try {
@@ -25,6 +28,27 @@ public class m_noticeview {
 			this.ps = this.con.prepareStatement(this.sql);
 			this.ps.setInt(1, nidx);		//nidx는 숫자니까 setInt
 			this.ps.executeUpdate();		//Query문을 실행!@!
+			
+			//해당 테이블에 맞는 컬럼 값을 select 함
+			this.sql = "select * from notice where nidx=? order by nidx desc";
+			this.ps = this.con.prepareStatement(this.sql);
+			this.ps.setInt(1, nidx);
+			this.rs = this.ps.executeQuery();
+			if(this.rs.next() == true) {	//해당 조건에 맞는 데이터가 있을 경우 
+				this.db_data = new ArrayList<>();
+				this.db_data.add(this.rs.getString("nidx"));
+				this.db_data.add(this.rs.getString("subject"));
+				this.db_data.add(this.rs.getString("writer"));
+				this.db_data.add(this.rs.getString("pw"));
+				this.db_data.add(this.rs.getString("texts"));
+				this.db_data.add(this.rs.getString("filenm"));
+				this.db_data.add(this.rs.getString("nfile"));
+				this.db_data.add(this.rs.getString("nview"));
+				this.db_data.add(this.rs.getString("ndate"));
+			}
+			
+			
+			
 		}catch (Exception e) {
 			
 		}finally {
--- web/src/main/java/notice/notice_view.java
+++ web/src/main/java/notice/notice_view.java
@@ -1,6 +1,7 @@
 package notice;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,8 +15,15 @@ public class notice_view extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int nidx = Integer.parseInt(request.getParameter("nidx"));
 //		System.out.println(nidx);
+		
+		//조회수 +1 증가 및 데이터를 출력 
 		m_noticeview mv = new m_noticeview();
 		mv.viewcount(nidx);
+		
+		//게시물 1개의 데이터 내용을 JSP로 전달 
+		ArrayList<String> notice_v = mv.db_data;
+//		System.out.println(notice_v);
+		request.setAttribute("notice_v", notice_v);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("./notice_view.jsp");
 		rd.forward(request, response);
